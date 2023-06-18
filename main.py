e97c7db4665912d5e748fb5bd18c07d5797f2d99
lidorani <lidor.azani@gmail.com>
2023-06-18 18:10:01 +0300

    notes added for part 2

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 
 # input: number of vertices, list of all subgraphs
-# output: number of motifs
+# output: list of motifs
 # function description: this function gets as input the number of vertices in a graph, then uses sub-functions
-#                       to count the number of non-isomorphic motifs that it contains.
+#                       to remove isomorphic motifs and at last has a list of unique motifs in graph.
 def get_motifs_part_a(n, all_subgraphs_list):
     # remove disconnected subgraphs
     connected_subgraphs = []
@@ -24,8 +24,8 @@ def get_motifs_part_a(n, all_subgraphs_list):
     return non_isomorphic_subgraphs
 
 
-# input: number of vertices
-# output: list of all sub-graphs for input
+# input: number of vertices, directed graph
+# output: list of all sub-graphs
 def generate_subgraphs(n, graph):
     if n == 1:
         graph.add_edge(0,0)
@@ -51,7 +51,7 @@ def generate_subgraphs(n, graph):
 
 # input: directed graph
 # output: boolean
-# function description: this function gets a directed graph, checks if it has all vertices, and check connectivity.
+# function description: this function gets a directed graph, checks if it has "n" vertices & check connectivity.
 def check_connectivity(graph, n):
     if graph.nodes.__len__() < n:
         return False
@@ -59,7 +59,8 @@ def check_connectivity(graph, n):
     return nx.is_connected(undirected_graph)
 
 
-# function description: this function outputs a list of unique patterns of graphs in given graphs list.
+# function description: this function gets a list of subgraphs, and outputs a list of *unique patterns of graphs
+#                       for this given graphs list.
 def get_non_isomorphic_subgraphs(subgraphs):
     non_isomorphic_subgraphs = []
     for graph in subgraphs:
@@ -74,6 +75,7 @@ def get_non_isomorphic_subgraphs(subgraphs):
     return non_isomorphic_subgraphs
 
 
+# function description: this function outputs the results of part a to a text files.
 def write_to_file_part_a(n, non_isomorphic_subgraphs):
     # Create a file to write the data
     filename = f"motifs_{n}.txt"
@@ -89,6 +91,7 @@ def write_to_file_part_a(n, non_isomorphic_subgraphs):
                 file.write(f"{edge[0]} {edge[1]}\n")
 
 
+# function description: this function plots running time as a function of n.
 def plot_graph(time_compare):
     n_values = range(1, len(time_compare) + 1)
     plt.plot(n_values, time_compare, marker='o')
@@ -130,6 +133,7 @@ def part_a():
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                         functions for part 2                      #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# function description: this function reads data from input file and parses it into variables.
 def get_input(name):
     # open file and read it's content
     with open("input.txt", "r") as file:
@@ -145,16 +149,21 @@ def get_input(name):
     return n, edges
 
 
+# function description: this function gets the highest value of vertex in graph.
+#                       it was created in order to know the size of input graph.
 def get_highest_vertex(edges):
     highest_vertex = max(max(edge) for edge in edges)
     return highest_vertex
 
 
+# function description: this function gets the lowest value of vertex in graph.
+#                       if it's 0, we know we need to relabel the vertices.
 def get_lowest_vertex(edges):
     lowest_vertex = min(min(edge) for edge in edges)
     return lowest_vertex
 
 
+# function description: this function gets a list of edges and a number of vertices & returns a graph.
 def create_graph_from_edges(num, edges):
     graph = nx.DiGraph()
     graph.add_nodes_from(range(1, num + 1))
@@ -178,7 +187,8 @@ def get_motifs_list_part_b(n, all_subgraphs_list):
     return non_isomorphic_subgraphs
 
 
-# function description: ???????????????????????????????????????????????????????????????????????????????
+# function description: this function gets a list of motifs & a list of subgraphs, then it counts how many appearances
+#                       of each motifs there are in subgraphs list.
 def count_num_of_motifs(motifs_list, subgraphs_list):
     graph_counts = {}
 
@@ -193,6 +203,7 @@ def count_num_of_motifs(motifs_list, subgraphs_list):
     return graph_counts
 
 
+# function description: this function outputs the results of part b to a text file.
 def write_to_file_part_b(n, graphs_list, num_of_appearances, filename):
     # Create a file to write the data
     with open(filename, 'w') as file:
@@ -211,9 +222,6 @@ def write_to_file_part_b(n, graphs_list, num_of_appearances, filename):
             file.write(f"count={count}\n\n")
 
 
-# function description: this function gets the input and parses it.
-#                       at first, it creates all subgraphs for given "n".
-#                       later it creates
 def part_b():
     # read input from text file and create a graph
     [n, edges] = get_input("input.txt")
@@ -240,5 +248,5 @@ def part_b():
 
 
 if __name__ == '__main__':
-    part_a()
+    # part_a()
     part_b()
